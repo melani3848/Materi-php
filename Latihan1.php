ea1f8b7c92494ddf48ece88b9fe14b8fbabd795a
melani3848 <132867305+melani3848@users.noreply.github.com>
2023-05-08 11:37:11 +0700

    Create Latihan1.php

---
+++ Latihan1.php
@@ -0,0 +1,21 @@
+<html>
+
+  <head>
+
+    <title>percobaan php</title>
+
+    </head>
+
+    <body>
+
+      
+
+    <?php echo ”<p>hello word</p>”;?>
+
+    <!-- echo untuk mencentak hasil-->
+
+    
+
+    </body>
+
+    </html>
